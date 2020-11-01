9ff624a5183ab3a9515546e32f738fdc8fb68b40
capeieboi <capeieboi@gmail.com>
2020-11-01 11:38:39 +0530

    first commit

---
+++ hive.py
@@ -0,0 +1,12 @@
+import time
+import paho.mqtt.client as paho
+import paho.mqtt.subscribe as subscribe
+broker="broker.hivemq.com"    
+client= paho.Client("client-001") 
+print("connecting to broker ",broker)
+client.connect(broker)
+client.loop_start()
+gulbul_var = ""
+while True:
+    msg = subscribe.simple("yashasbigboi/1", hostname=broker)
+    gulbul_var = str(msg.payload.decode("utf-8"))
